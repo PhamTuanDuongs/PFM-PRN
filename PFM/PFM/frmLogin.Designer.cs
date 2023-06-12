515226bcf644970e86b7b76440f307989a701eb6
Phạm  Tuấn Dương <githubtokenss@gmail.com>
2023-06-13 00:59:07 +0700

    feat: login

---
+++ PFM/PFM/frmLogin.Designer.cs
@@ -0,0 +1,161 @@
+﻿namespace PFM
+{
+    partial class frmLogin
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            components = new System.ComponentModel.Container();
+            checkBox1 = new CheckBox();
+            linkLabel1 = new LinkLabel();
+            btnClearFields = new Button();
+            btnSignIn = new Button();
+            label2 = new Label();
+            label1 = new Label();
+            txtPassword = new TextBox();
+            txtUsername = new TextBox();
+            errorProvider1 = new ErrorProvider(components);
+            ((System.ComponentModel.ISupportInitialize)errorProvider1).BeginInit();
+            SuspendLayout();
+            // 
+            // checkBox1
+            // 
+            checkBox1.AutoSize = true;
+            checkBox1.Location = new Point(485, 169);
+            checkBox1.Name = "checkBox1";
+            checkBox1.Size = new Size(67, 24);
+            checkBox1.TabIndex = 25;
+            checkBox1.Text = "Show";
+            checkBox1.UseVisualStyleBackColor = true;
+            checkBox1.CheckedChanged += checkBox1_CheckedChanged;
+            // 
+            // linkLabel1
+            // 
+            linkLabel1.AutoSize = true;
+            linkLabel1.LinkColor = Color.FromArgb(64, 64, 64);
+            linkLabel1.Location = new Point(205, 340);
+            linkLabel1.Name = "linkLabel1";
+            linkLabel1.Size = new Size(226, 20);
+            linkLabel1.TabIndex = 24;
+            linkLabel1.TabStop = true;
+            linkLabel1.Text = "Don’t have any account? Sign Up";
+            linkLabel1.Click += linkLabel1_Click;
+            // 
+            // btnClearFields
+            // 
+            btnClearFields.Location = new Point(378, 212);
+            btnClearFields.Name = "btnClearFields";
+            btnClearFields.Size = new Size(101, 25);
+            btnClearFields.TabIndex = 23;
+            btnClearFields.Text = "Clear fields";
+            btnClearFields.UseVisualStyleBackColor = true;
+            btnClearFields.Click += btnClearFields_Click;
+            // 
+            // btnSignIn
+            // 
+            btnSignIn.Font = new Font("Segoe UI", 10.8F, FontStyle.Regular, GraphicsUnit.Point);
+            btnSignIn.Location = new Point(260, 269);
+            btnSignIn.Name = "btnSignIn";
+            btnSignIn.Size = new Size(142, 47);
+            btnSignIn.TabIndex = 22;
+            btnSignIn.Text = "Sign in";
+            btnSignIn.UseVisualStyleBackColor = true;
+            btnSignIn.Click += btnSignIn_Click;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Font = new Font("Segoe UI", 10.8F, FontStyle.Regular, GraphicsUnit.Point);
+            label2.Location = new Point(51, 165);
+            label2.Name = "label2";
+            label2.Size = new Size(87, 25);
+            label2.TabIndex = 21;
+            label2.Text = "Password";
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Segoe UI", 10.8F, FontStyle.Regular, GraphicsUnit.Point);
+            label1.Location = new Point(51, 104);
+            label1.Name = "label1";
+            label1.Size = new Size(91, 25);
+            label1.TabIndex = 20;
+            label1.Text = "Username";
+            // 
+            // txtPassword
+            // 
+            txtPassword.Location = new Point(188, 166);
+            txtPassword.Name = "txtPassword";
+            txtPassword.Size = new Size(291, 27);
+            txtPassword.TabIndex = 19;
+            txtPassword.UseSystemPasswordChar = true;
+            txtPassword.Validating += txtPassword_Validating;
+            // 
+            // txtUsername
+            // 
+            txtUsername.Location = new Point(188, 105);
+            txtUsername.Name = "txtUsername";
+            txtUsername.Size = new Size(291, 27);
+            txtUsername.TabIndex = 18;
+            txtUsername.Validating += txtUsername_Validating;
+            // 
+            // errorProvider1
+            // 
+            errorProvider1.ContainerControl = this;
+            // 
+            // frmLogin
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(602, 464);
+            Controls.Add(checkBox1);
+            Controls.Add(linkLabel1);
+            Controls.Add(btnClearFields);
+            Controls.Add(btnSignIn);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            Controls.Add(txtPassword);
+            Controls.Add(txtUsername);
+            Name = "frmLogin";
+            Text = "Login";
+            ((System.ComponentModel.ISupportInitialize)errorProvider1).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private CheckBox checkBox1;
+        private LinkLabel linkLabel1;
+        private Button btnClearFields;
+        private Button btnSignIn;
+        private Label label2;
+        private Label label1;
+        private TextBox txtPassword;
+        private TextBox txtUsername;
+        private ErrorProvider errorProvider1;
+    }
+}
---
+++ PFM/PFM/frmLogin.cs
@@ -0,0 +1,106 @@
+using PFM.Models;
+using System.Text.RegularExpressions;
+using System.Windows.Forms;
+
+namespace PFM
+{
+    public partial class frmLogin : Form
+    {
+        PFMContext context;
+        public frmLogin()
+        {
+            context = new PFMContext();
+            InitializeComponent();
+        }
+
+        private void linkLabel1_Click(object sender, EventArgs e)
+        {
+            frmSignup s = new frmSignup();
+            s.Show();
+            this.Hide();
+        }
+
+        private void btnClearFields_Click(object sender, EventArgs e)
+        {
+            txtUsername.Text = string.Empty;
+            txtPassword.Text = string.Empty;
+        }
+
+        private void btnSignIn_Click(object sender, EventArgs e)
+        {
+            string username = txtUsername.Text;
+            string password = txtPassword.Text;
+            Account account = context.Accounts.FirstOrDefault(x => x.Username == username && x.Password == password);
+            if (account != null)
+            {
+                LoginInfo.Id = account.Id;
+                LoginInfo.Username = txtUsername.Text;
+                MessageBox.Show("Login Successfully");
+                frmMain dashboard = new frmMain();
+                dashboard.Show();
+                this.Hide();
+            }
+            else
+            {
+                MessageBox.Show("Login Fail");
+
+            }
+        }
+
+        private void checkBox1_CheckedChanged(object sender, EventArgs e)
+        {
+            if (checkBox1.Checked)
+            {
+                txtPassword.UseSystemPasswordChar = false;
+            }
+            else
+            {
+                txtPassword.UseSystemPasswordChar = true;
+            }
+        }
+
+        private void txtUsername_Validating(object sender, System.ComponentModel.CancelEventArgs e)
+        {
+            if (string.IsNullOrWhiteSpace(txtUsername.Text))
+            {
+                e.Cancel = true;
+                txtUsername.Focus();
+                errorProvider1.SetError(txtUsername, "Name should not be left blank!");
+            }
+            else if (!Regex.Match(txtUsername.Text, @"^[0-9\\-\\+]{10}$").Success)
+            {
+                e.Cancel = true;
+                txtUsername.Focus();
+                errorProvider1.SetError(txtUsername, "Username must be a phone number with length 10 digits");
+            }
+            else
+            {
+                e.Cancel = false;
+                errorProvider1.SetError(txtUsername, "");
+
+            }
+        }
+
+        private void txtPassword_Validating(object sender, System.ComponentModel.CancelEventArgs e)
+        {
+            if (string.IsNullOrWhiteSpace(txtPassword.Text))
+            {
+                e.Cancel = true;
+                txtPassword.Focus();
+                errorProvider1.SetError(txtPassword, "Name should not be left blank!");
+            }
+            else if (txtPassword.Text.Length > 50)
+            {
+                e.Cancel = true;
+                txtPassword.Focus();
+                errorProvider1.SetError(txtPassword, "Password should not larger than 50 characters");
+            }
+            else
+            {
+                e.Cancel = false;
+                errorProvider1.SetError(txtPassword, "");
+
+            }
+        }
+    }
+}
