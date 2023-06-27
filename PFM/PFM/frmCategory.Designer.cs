c9a597d5f88aa5a27ed663cf4835853dc1a856e3
Phạm  Tuấn Dương <githubtokenss@gmail.com>
2023-06-27 23:23:35 +0700

    feat: add-view category

---
+++ PFM/PFM/frmCategory.Designer.cs
@@ -0,0 +1,238 @@
+﻿namespace PFM
+{
+    partial class frmCategory
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            components = new System.ComponentModel.Container();
+            rdExpense = new RadioButton();
+            rdIncome = new RadioButton();
+            label1 = new Label();
+            dtCategory = new DataGridView();
+            btnAdd = new Button();
+            label2 = new Label();
+            txtCategoryName = new TextBox();
+            button1 = new Button();
+            btnDelete = new Button();
+            validate = new ErrorProvider(components);
+            label3 = new Label();
+            txtId = new TextBox();
+            label7 = new Label();
+            label4 = new Label();
+            ((System.ComponentModel.ISupportInitialize)dtCategory).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)validate).BeginInit();
+            SuspendLayout();
+            // 
+            // rdExpense
+            // 
+            rdExpense.AutoSize = true;
+            rdExpense.Checked = true;
+            rdExpense.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point);
+            rdExpense.Location = new Point(577, 171);
+            rdExpense.Name = "rdExpense";
+            rdExpense.Size = new Size(103, 32);
+            rdExpense.TabIndex = 0;
+            rdExpense.TabStop = true;
+            rdExpense.Text = "Expense";
+            rdExpense.UseVisualStyleBackColor = true;
+            rdExpense.CheckedChanged += rdExpense_CheckedChanged;
+            // 
+            // rdIncome
+            // 
+            rdIncome.AutoSize = true;
+            rdIncome.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point);
+            rdIncome.Location = new Point(1098, 171);
+            rdIncome.Name = "rdIncome";
+            rdIncome.Size = new Size(97, 32);
+            rdIncome.TabIndex = 1;
+            rdIncome.Text = "Income";
+            rdIncome.UseVisualStyleBackColor = true;
+            rdIncome.CheckedChanged += rdIncome_CheckedChanged;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Segoe UI Semibold", 18F, FontStyle.Bold | FontStyle.Italic, GraphicsUnit.Point);
+            label1.Location = new Point(605, 35);
+            label1.Name = "label1";
+            label1.Size = new Size(139, 41);
+            label1.TabIndex = 2;
+            label1.Text = "Category";
+            // 
+            // dtCategory
+            // 
+            dtCategory.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dtCategory.Location = new Point(577, 228);
+            dtCategory.Name = "dtCategory";
+            dtCategory.RowHeadersWidth = 51;
+            dtCategory.RowTemplate.Height = 29;
+            dtCategory.Size = new Size(904, 485);
+            dtCategory.TabIndex = 3;
+            dtCategory.CellClick += dtCategory_CellClick;
+            // 
+            // btnAdd
+            // 
+            btnAdd.AutoSize = true;
+            btnAdd.BackColor = Color.LightGreen;
+            btnAdd.Font = new Font("Segoe UI", 10.8F, FontStyle.Regular, GraphicsUnit.Point);
+            btnAdd.Location = new Point(16, 461);
+            btnAdd.Name = "btnAdd";
+            btnAdd.Size = new Size(110, 35);
+            btnAdd.TabIndex = 8;
+            btnAdd.Text = "Add";
+            btnAdd.UseVisualStyleBackColor = false;
+            btnAdd.Click += btnAdd_Click;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point);
+            label2.Location = new Point(60, 316);
+            label2.Name = "label2";
+            label2.Size = new Size(64, 28);
+            label2.TabIndex = 7;
+            label2.Text = "Name";
+            // 
+            // txtCategoryName
+            // 
+            txtCategoryName.Location = new Point(178, 320);
+            txtCategoryName.Name = "txtCategoryName";
+            txtCategoryName.Size = new Size(256, 27);
+            txtCategoryName.TabIndex = 6;
+            txtCategoryName.Validating += txtCategoryName_Validating;
+            // 
+            // button1
+            // 
+            button1.BackColor = SystemColors.Highlight;
+            button1.Font = new Font("Segoe UI", 10.8F, FontStyle.Regular, GraphicsUnit.Point);
+            button1.Location = new Point(210, 461);
+            button1.Name = "button1";
+            button1.Size = new Size(110, 35);
+            button1.TabIndex = 9;
+            button1.Text = "Update";
+            button1.UseVisualStyleBackColor = false;
+            button1.Click += button1_Click;
+            // 
+            // btnDelete
+            // 
+            btnDelete.BackColor = Color.IndianRed;
+            btnDelete.Font = new Font("Segoe UI", 10.8F, FontStyle.Regular, GraphicsUnit.Point);
+            btnDelete.Location = new Point(383, 461);
+            btnDelete.Name = "btnDelete";
+            btnDelete.Size = new Size(110, 35);
+            btnDelete.TabIndex = 10;
+            btnDelete.Text = "Delete";
+            btnDelete.UseVisualStyleBackColor = false;
+            btnDelete.Click += btnDelete_Click;
+            // 
+            // validate
+            // 
+            validate.ContainerControl = this;
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point);
+            label3.Location = new Point(68, 250);
+            label3.Name = "label3";
+            label3.Size = new Size(31, 28);
+            label3.TabIndex = 11;
+            label3.Text = "ID";
+            // 
+            // txtId
+            // 
+            txtId.Location = new Point(186, 254);
+            txtId.Name = "txtId";
+            txtId.ReadOnly = true;
+            txtId.Size = new Size(248, 27);
+            txtId.TabIndex = 12;
+            // 
+            // label7
+            // 
+            label7.AutoSize = true;
+            label7.ForeColor = Color.Red;
+            label7.Location = new Point(94, 257);
+            label7.Name = "label7";
+            label7.Size = new Size(15, 20);
+            label7.TabIndex = 30;
+            label7.Text = "*";
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.ForeColor = Color.Red;
+            label4.Location = new Point(120, 327);
+            label4.Name = "label4";
+            label4.Size = new Size(15, 20);
+            label4.TabIndex = 31;
+            label4.Text = "*";
+            // 
+            // frmCategory
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(1484, 712);
+            Controls.Add(label4);
+            Controls.Add(label7);
+            Controls.Add(txtId);
+            Controls.Add(label3);
+            Controls.Add(btnDelete);
+            Controls.Add(button1);
+            Controls.Add(btnAdd);
+            Controls.Add(label2);
+            Controls.Add(txtCategoryName);
+            Controls.Add(dtCategory);
+            Controls.Add(label1);
+            Controls.Add(rdIncome);
+            Controls.Add(rdExpense);
+            Name = "frmCategory";
+            Text = "Category";
+            Load += frmViewCategory_Load_1;
+            ((System.ComponentModel.ISupportInitialize)dtCategory).EndInit();
+            ((System.ComponentModel.ISupportInitialize)validate).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private RadioButton rdExpense;
+        private RadioButton rdIncome;
+        private Label label1;
+        private DataGridView dtCategory;
+        private Button btnAdd;
+        private Label label2;
+        private TextBox txtCategoryName;
+        private Button button1;
+        private Button btnDelete;
+        private ErrorProvider validate;
+        private Label label3;
+        private TextBox txtId;
+        private Label label4;
+        private Label label7;
+    }
+}
---
+++ PFM/PFM/frmCategory.cs
@@ -0,0 +1,190 @@
+﻿using PFM.Models;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PFM
+{
+    public partial class frmCategory : Form
+    {
+        PFMContext context;
+        public frmCategory()
+        {
+            context = new PFMContext();
+            InitializeComponent();
+            dtCategory.AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.Fill;
+            dtCategory.AutoSizeRowsMode = DataGridViewAutoSizeRowsMode.AllCells;
+            rdExpense.Checked = true;
+            rdExpense.Checked = false;
+        }
+
+        private void LoaddataExpense()
+        {
+            dtCategory.DataSource = (
+                from c in context.Categories
+                where c.CategoryType == 0
+                select new
+                {
+                    No = c.Id,
+                    c.CategoryName,
+                }
+                ).ToList();
+        }
+        private void rdExpense_CheckedChanged(object sender, EventArgs e)
+        {
+            if (rdExpense.Checked)
+            {
+                LoaddataExpense();
+            }
+        }
+
+        private void LoadDataWithCondition()
+        {
+            if (rdExpense.Checked)
+            {
+                LoaddataExpense();
+            }
+            else
+            {
+                LoaddataIncome();
+            }
+        }
+        private void LoaddataIncome()
+        {
+            dtCategory.DataSource = (
+                from c in context.Categories
+                where c.CategoryType == 1
+                select new
+                {
+                    No = c.Id,
+                    CategoryName = c.CategoryName,
+                }
+                ).ToList();
+        }
+        private void rdIncome_CheckedChanged(object sender, EventArgs e)
+        {
+            if (rdIncome.Checked)
+            {
+                LoaddataIncome();
+            }
+        }
+
+        private void txtCategoryName_Validating(object sender, CancelEventArgs e)
+        {
+            if (string.IsNullOrWhiteSpace(txtCategoryName.Text))
+            {
+                e.Cancel = true;
+                txtCategoryName.Focus();
+                validate.SetError(txtCategoryName, "Name should not be left blank!");
+            }
+            else if (txtCategoryName.Text.Length > 150)
+            {
+                e.Cancel = true;
+                txtCategoryName.Focus();
+                validate.SetError(txtCategoryName, "The length of income name should not be larger than 150 characters");
+            }
+            else
+            {
+                e.Cancel = false;
+                validate.SetError(txtCategoryName, "");
+            }
+        }
+
+        private void btnAdd_Click(object sender, EventArgs e)
+        {
+            if (ValidateChildren())
+            {
+                int categoryType;
+                if (rdExpense.Checked)
+                {
+                    categoryType = 0;
+                }
+                else
+                {
+                    categoryType = 1;
+                }
+                string name = txtCategoryName.Text;
+                Category checkCategory = context.Categories.FirstOrDefault(x => x.CategoryName.Equals(name));
+                if (checkCategory == null)
+                {
+                    Category category = new Category();
+                    category.CategoryName = name;
+                    category.CategoryType = categoryType;
+                    context.Categories.Add(category);
+                    context.SaveChanges();
+                    MessageBox.Show("A new category added successfully");
+                    LoadDataWithCondition();
+                }
+                else
+                {
+                    MessageBox.Show("This category name exits ");
+                }
+            }
+        }
+
+
+        private void frmViewCategory_Load_1(object sender, EventArgs e)
+        {
+            rdExpense.Checked = true;
+            LoaddataExpense();
+        }
+
+        private void dtCategory_CellClick(object sender, DataGridViewCellEventArgs e)
+        {
+            if (e.RowIndex < 0) { return; }
+            txtId.Text = dtCategory.Rows[e.RowIndex].Cells[0].FormattedValue.ToString();
+            txtCategoryName.Text = dtCategory.Rows[e.RowIndex].Cells[1].FormattedValue.ToString();
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (string.IsNullOrEmpty(txtCategoryName.Text) || string.IsNullOrEmpty(txtId.Text))
+            {
+                MessageBox.Show("You must choose a category to update");
+            }
+            else
+            {
+                Category oldCategory = context.Categories.FirstOrDefault(c => c.Id == Convert.ToInt32(txtId.Text));
+                if (oldCategory != null)
+                {
+                    oldCategory.CategoryName = txtCategoryName.Text;
+                    context.SaveChanges();
+                    MessageBox.Show("Update category successfully");
+                    LoadDataWithCondition();
+                }
+                else
+                {
+                    MessageBox.Show("This category does not exit to update");
+                }
+
+            }
+        }
+
+        private void btnDelete_Click(object sender, EventArgs e)
+        {
+
+            if (string.IsNullOrEmpty(txtCategoryName.Text) || string.IsNullOrEmpty(txtId.Text))
+            {
+                MessageBox.Show("You must choose a category to delete");
+            }
+            else
+            {
+                Category oldCategory = context.Categories.FirstOrDefault(c => c.Id == Convert.ToInt32(txtId.Text));
+                context.Categories.Remove(oldCategory);
+                context.SaveChanges();
+                MessageBox.Show("Delete category successfully");
+                txtCategoryName.Text = string.Empty;
+                txtId.Text = string.Empty;
+                LoadDataWithCondition();
+            }
+        }
+    }
+}
